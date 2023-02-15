05835bcba313cf8d35d100a9bf94546c1c799556
Gastón Konstantinides <gastonkons@gmail.com>
2023-02-14 23:58:14 -0300

    test routes

--- index.mjs
+++ index.mjs
@@ -1,13 +1,22 @@
 import express from 'express'
+import cors from 'cors'
 
 const app = express()
+const PORT = process.env.PORT || 5000
 
+app.use(cors())
+app.use(express.json())
 app.get("/", (_, res) => {
   res.send("Express on Vercel")
 })
 
-app.listen(5000, () => {
-  console.log("Running on port 5000")
+app.get("/port", (_, res) => {
+  res.json({ port: PORT })
+})
+
+
+app.listen(PORT, () => {
+  console.log(`Running on port ${PORT}`)
 })
 
 export default app
